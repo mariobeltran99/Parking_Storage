ef584b81618d9ccdb1e6c66f70680fdfe5e5bf7a
Ernest2200 <ernesto21rosales@gmail.com>
2020-02-25 20:00:59 -0600

    Corrección de nombre apartado información

--- Sistema/ParkingStorage_System/ParkingStorage_System/Informacion.Designer.cs
+++ Sistema/ParkingStorage_System/ParkingStorage_System/Informacion.Designer.cs
@@ -63,7 +63,7 @@
             this.label1.ForeColor = System.Drawing.Color.White;
             this.label1.Location = new System.Drawing.Point(49, 9);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(223, 18);
+            this.label1.Size = new System.Drawing.Size(284, 23);
             this.label1.TabIndex = 1;
             this.label1.Text = "Parking Storage - Acerca de ";
             // 
@@ -92,7 +92,7 @@
             this.linkLabel1.AutoSize = true;
             this.linkLabel1.Location = new System.Drawing.Point(497, 369);
             this.linkLabel1.Name = "linkLabel1";
-            this.linkLabel1.Size = new System.Drawing.Size(79, 18);
+            this.linkLabel1.Size = new System.Drawing.Size(103, 23);
             this.linkLabel1.TabIndex = 2;
             this.linkLabel1.TabStop = true;
             this.linkLabel1.Text = "Regresar ";
@@ -103,7 +103,7 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(26, 131);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(141, 18);
+            this.label2.Size = new System.Drawing.Size(179, 23);
             this.label2.TabIndex = 3;
             this.label2.Text = "Desarrollado por:";
             // 
@@ -113,7 +113,7 @@
             this.label3.Font = new System.Drawing.Font("Lucida Sans", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.Location = new System.Drawing.Point(118, 372);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(296, 15);
+            this.label3.Size = new System.Drawing.Size(363, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "© 2020 - Parking Storage - Derechos Reservados";
             // 
@@ -122,7 +122,7 @@
             this.label4.AutoSize = true;
             this.label4.Location = new System.Drawing.Point(49, 172);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(153, 18);
+            this.label4.Size = new System.Drawing.Size(199, 23);
             this.label4.TabIndex = 5;
             this.label4.Text = "Mario Josué Beltrán";
             // 
@@ -131,7 +131,7 @@
             this.label5.AutoSize = true;
             this.label5.Location = new System.Drawing.Point(49, 204);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(157, 18);
+            this.label5.Size = new System.Drawing.Size(197, 23);
             this.label5.TabIndex = 6;
             this.label5.Text = "Daniela Sara García";
             // 
@@ -140,7 +140,7 @@
             this.label6.AutoSize = true;
             this.label6.Location = new System.Drawing.Point(49, 235);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(200, 18);
+            this.label6.Size = new System.Drawing.Size(262, 23);
             this.label6.TabIndex = 7;
             this.label6.Text = "Edgar Ernesto Hernández";
             // 
@@ -149,7 +149,7 @@
             this.label7.AutoSize = true;
             this.label7.Location = new System.Drawing.Point(49, 268);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(151, 18);
+            this.label7.Size = new System.Drawing.Size(187, 23);
             this.label7.TabIndex = 8;
             this.label7.Text = "José Ricardo Mejía";
             // 
@@ -158,13 +158,13 @@
             this.label8.AutoSize = true;
             this.label8.Location = new System.Drawing.Point(49, 301);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(175, 18);
+            this.label8.Size = new System.Drawing.Size(237, 23);
             this.label8.TabIndex = 9;
-            this.label8.Text = "Patrik Ernesto Rosales";
+            this.label8.Text = "Patrick Ernesto Rosales";
             // 
             // Informacion
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 23F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.LightGray;
             this.ClientSize = new System.Drawing.Size(588, 396);
