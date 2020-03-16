55c64de3f159041f5e32fc07d1709ae1ce989807
mario <mariobeltran1999@gmail.com>
2020-03-15 22:01:02 -0600

    Sistema v4.8.0

--- Sistema/ParkingStorage_System/ParkingStorage_System/Tickets.cs
+++ Sistema/ParkingStorage_System/ParkingStorage_System/Tickets.cs
@@ -12,9 +12,32 @@ namespace ParkingStorage_System
 {
     public partial class Tickets : Form
     {
+
         public Tickets()
         {
             InitializeComponent();
+            cmbfilter.SelectedIndex = 0;
         }
+
+        private void cmbfilter_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(cmbfilter.SelectedIndex == 0)
+            {
+                label3.Text = "Búsqueda por sección y código de Ticket";
+            }else if(cmbfilter.SelectedIndex == 1)
+            {
+                label3.Text = "Búsqueda por fecha y código de Ticket";
+            }else if(cmbfilter.SelectedIndex == 2)
+            {
+                label3.Text = "Búsqueda por fecha y código de Ticket";
+            }else if(cmbfilter.SelectedIndex == 3)
+            {
+                label3.Text = "Búsqueda por nombre, apellido, fecha, código de empleado o ticket";
+            }else if(cmbfilter.SelectedIndex == 4)
+            {
+                label3.Text = "Búsqueda por nombre, apellido, fecha, código de empleado o ticket";
+            }
+        }
+
     }
 }
